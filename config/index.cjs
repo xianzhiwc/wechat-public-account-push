7d619f57a2baa57d712f5a10e1da7628f60c80e1
xianzhiwc <39001004+xianzhiwc@users.noreply.github.com>
2022-09-30 11:38:07 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -67,7 +67,7 @@ const USER_CONFIG = {
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'op4Fg55OdJUSJ4IBKJ7Lfkn5Aexk',
       // 你想对他发送的模板消息的模板ID
-      useTemplateId: '8HUIEXmKoDBzpxXMSHPNPKJYliouqejhmxxhl_5H2Po',
+      useTemplateId: 'sFSctbLqJ9LJ2f7rECePbLp9w6SqgFbvkYQi5l4peck',
       // 所在省份
       province: '北京',
       // 所在城市
